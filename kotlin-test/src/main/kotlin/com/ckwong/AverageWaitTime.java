07dc93ba7d91ec20bd9f0295afc864b91cc4a5dd
Chishun Kwong <chishunkwong@yahoo.com>
2021-03-08 15:28:33 -0500

    clean up

--- kotlin-test/src/main/kotlin/com/ckwong/AverageWaitTime.java
+++ kotlin-test/src/main/kotlin/com/ckwong/AverageWaitTime.java
@@ -9,6 +9,7 @@ public class AverageWaitTime {
         final AtomicInteger actualStart = new AtomicInteger(customers[0][0]);
         double answer = Arrays.stream(customers).mapToInt(
                 arr -> {
+                    // perhaps the customer walked in and no outstanding order was being made
                     actualStart.set(Math.max(arr[0], actualStart.get()));
                     int waitTime = actualStart.get() + arr[1] - arr[0];
                     actualStart.addAndGet(arr[1]);
@@ -20,11 +21,7 @@ public class AverageWaitTime {
 
     public static void main(String[] args) {
         AverageWaitTime sol = new AverageWaitTime();
-        int[][] customers = new int[4][2];
-        customers[0] = new int[]{5, 2};
-        customers[1] = new int[]{5, 4};
-        customers[2] = new int[]{10, 3};
-        customers[3] = new int[]{20, 1};
+        int[][] customers = {{5, 2}, {5, 4}, {10, 3}, {20, 1}};
         System.out.println(sol.averageWaitingTime(customers));
     }
 }
