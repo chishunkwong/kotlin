09a1d86b94c4a44f1721cf1dd98e919805736e21
Chishun Kwong <chishunkwong@yahoo.com>
2021-03-08 12:00:26 -0500

    solution to kth smallest in bst

--- kotlin-test/src/main/kotlin/com/ckwong/KthSmallestInBST.java
+++ kotlin-test/src/main/kotlin/com/ckwong/KthSmallestInBST.java
@@ -1,5 +1,8 @@
 package com.ckwong;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class KthSmallestInBST {
 
     static class TreeNode {
@@ -19,13 +22,56 @@ public class KthSmallestInBST {
             this.left = left;
             this.right = right;
         }
+
+        @Override
+        public String toString() {
+            return String.valueOf(val);
+        }
     }
 
     /**
      * Given the root of a BST, find the kth (1-indexed) smallest element in the tree.
      */
     public int kthSmallest(TreeNode root, int k) {
-        return 0;
+        return kthSmallestInternal(root, k, new HashMap<Integer, Integer[]>());
+    }
+
+    /**
+     * helper function for above, with a memory of what we already counted left-right-wise
+     * (I am assuming that all the nodes in the BST are unique)
+     */
+    private int kthSmallestInternal(TreeNode node, int k, Map<Integer, Integer[]> memory) {
+        Integer[] leftRight = countLeftRight(node, memory);
+        // System.out.println(leftRight[0] + ", " + leftRight[1]);
+        if (k == leftRight[0] + 1) {
+            return node.val;
+        } else if (k <= leftRight[0]) {
+            return kthSmallestInternal(node.left, k, memory);
+        } else {
+            // System.out.println(node);
+            return kthSmallestInternal(node.right, k - 1 - leftRight[0], memory);
+        }
+    }
+
+    private Integer[] countLeftRight(TreeNode node, Map<Integer, Integer[]> memory) {
+        Integer[] already = memory.get(node.val);
+        if (already != null) {
+            System.out.println("Hit " + node.val);
+            return already;
+        }
+        int left = 0;
+        int right = 0;
+        if (node.left != null) {
+            Integer[] leftRight = countLeftRight(node.left, memory);
+            left = 1 + leftRight[0] + leftRight[1];
+        }
+        if (node.right != null) {
+            Integer[] leftRight = countLeftRight(node.right, memory);
+            right = 1 + leftRight[0] + leftRight[1];
+        }
+        Integer[] answer = new Integer[]{left, right};
+        memory.put(node.val, answer);
+        return answer;
     }
 
     public static void main(String[] args) {
@@ -41,8 +87,8 @@ public class KthSmallestInBST {
         TreeNode root = new TreeNode(5);
         root.right = six;
         root.left = three;
-        sol.kthSmallest(root, 3);
+        Integer answer = sol.kthSmallest(root, 4);
 
-        System.out.println("Hello Java");
+        System.out.println("Answer: " + answer);
     }
 }
