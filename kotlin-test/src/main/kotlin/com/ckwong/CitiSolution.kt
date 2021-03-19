30878be98a9d999431ca52edd02b9f5f0df50171
Chishun Kwong <chishunkwong@yahoo.com>
2021-03-19 17:58:08 -0400

    citiciti

--- kotlin-test/src/main/kotlin/com/ckwong/CitiSolution.kt
+++ kotlin-test/src/main/kotlin/com/ckwong/CitiSolution.kt
@@ -6,32 +6,40 @@ class CitiSolution {
 
     fun solution(S: String, C: IntArray): Int {
         var lastChar: Char = S[0]
-        var minCost = C[0]
+        var maxCost = C[0]
         var sumCost = C[0]
         var sameCount = 1
         var total: Int = 0
-        (1 until S.length).forEach { i ->
-            val c = S[i]
-            val cost = C[i]
-            if (c == lastChar) {
-                if (cost < minCost) {
-                    minCost = cost
-                }
-                sumCost += cost
-                sameCount++
-            } else {
-                if (sameCount > 1) {
-                    total += (sumCost - minCost)
-                }
-                lastChar = S[i]
-                minCost = cost
-                sumCost = cost
-                sameCount = 1
+        fun reset(i: Int) {
+            val c = C[i]
+            lastChar = S[i]
+            maxCost = c
+            sumCost = c
+            sameCount = 1
+        }
+        fun update(i: Int) {
+            val c = C[i]
+            if (c > maxCost) {
+                maxCost = c
+            }
+            sumCost += c
+            sameCount++
+        }
+        fun calc() {
+            if (sameCount > 1) {
+                total += (sumCost - maxCost)
             }
         }
-        if (sameCount > 1) {
-            total += (sumCost - minCost)
+        (1 until S.length).forEach { i ->
+            val c = S[i]
+            if (c == lastChar) {
+                update(i)
+            } else {
+                calc()
+                reset(i)
+            }
         }
+        calc()
         return total
     }
 
@@ -39,7 +47,8 @@ class CitiSolution {
         @JvmStatic
         fun main(args: Array<String>) {
             val sol = CitiSolution()
-            print(sol.solution("aaabbcc", intArrayOf(1, 2, 0, 1, 2, 1, 2)))
+            println(sol.solution("aaabbcc", intArrayOf(1, 2, 0, 1, 2, 3, 4)))
+            println(sol.solution("abcb", intArrayOf(1, 2, 3, 4)))
         }
     }
 }
--- kotlin-test/src/main/kotlin/com/ckwong/CitiSolutionJava.java
+++ kotlin-test/src/main/kotlin/com/ckwong/CitiSolutionJava.java
@@ -13,9 +13,73 @@ class CitiSolutionJava {
         return ans;
     }
 
+    public int wordMachine(String S) {
+        List<Integer> numbers = new ArrayList();
+        String[] parts = S.split(" ");
+        for (String part : parts) {
+            if (part.equals("POP")) {
+                if (numbers.isEmpty()) {
+                    // throw new IllegalStateException("No number to pop");
+                    return -1;
+                }
+                numbers.remove(numbers.size() - 1);
+            } else if (part.equals("DUP")){
+                if (numbers.isEmpty()) {
+                    // throw new IllegalStateException("No number to dup");
+                    return -1;
+                }
+                numbers.add(numbers.get(numbers.size() - 1));
+            } else if (part.equals("+")){
+                int len = numbers.size();
+                if (len < 2) {
+                    // throw new IllegalStateException("Not two numbers left to add");
+                    return -1;
+                }
+                int first = numbers.remove(numbers.size() - 1);
+                int second = numbers.remove(numbers.size() - 1);
+                if (first + second < 0) {
+                    throw new IllegalStateException("overflow");
+                    // return -1;
+                }
+                numbers.add(first + second);
+            } else if (part.equals("-")){
+                int len = numbers.size();
+                if (len < 2) {
+                    // throw new IllegalStateException("Not two numbers left to subtract");
+                    return -1;
+                }
+                int first = numbers.remove(numbers.size() - 1);
+                int second = numbers.remove(numbers.size() - 1);
+                if (first < second) {
+                    // throw new IllegalStateException("subtraction will result in a negative number");
+                    return -1;
+                }
+                numbers.add(first - second);
+            } else {
+                // better be a non-negative integer
+                int number = Integer.parseInt(part);
+                if (number < 0) {
+                    // throw new IllegalArgumentException("number less than 0");
+                    return -1;
+                }
+                numbers.add(number);
+            }
+        }
+        if (numbers.isEmpty()) {
+            // throw new IllegalStateException("No numbers left");
+            return -1;
+        }
+        return numbers.remove(numbers.size() - 1);
+    }
+
     public static void main(String[] args) {
         CitiSolutionJava sol = new CitiSolutionJava();
-        int answer = sol.solution(new int[] {-3, 1, -2, 2});
-        System.out.println(answer);
+        // int answer = sol.solution(new int[] {-3, 1, -2, 2});
+        // System.out.println(answer);
+        System.out.println(sol.wordMachine("2147483646 2147483647 +"));
+        // System.out.println(sol.wordMachine("4 5 6 - 7 +"));
+        // System.out.println(sol.wordMachine("13 DUP 4 POP 5 DUP + DUP + -"));
+        // System.out.println(sol.wordMachine("5 6 + -"));
+        // System.out.println(sol.wordMachine("3 DUP 5 - -"));
     }
 }
